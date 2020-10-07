66edb3a4a024039a885375022b4165928312e6f4
cteer458 <conorteer@hotmail.co.uk>
2020-10-07 22:38:44 +0100

    updated with FW FB change

--- run-scripts.bat
+++ run-scripts.bat
@@ -1,9 +1,10 @@
 .\simc_vs2019.exe .\frost\legendaries.simc
+.\simc_vs2019.exe .\frost\legendaries-2-target.simc
 .\simc_vs2019.exe .\frost\soulbinds-week-1.simc
 .\simc_vs2019.exe .\frost\soulbinds-week-4.simc
 .\simc_vs2019.exe .\frost\soulbinds-max.simc
-.\simc_vs2019.exe .\frost\stats.simc
 .\simc_vs2019.exe .\frost\races.simc
 .\simc_vs2019.exe .\frost\covenant-abilities.simc
 .\simc_vs2019.exe .\frost\relics.simc
 .\simc_vs2019.exe .\frost\soulbinds-week-4-2-target.simc
+.\simc_vs2019.exe .\frost\stats.simc
