8509ad46a67efd559c9d96edaf0eb7a8d0a98e01
cteer458 <conorteer@hotmail.co.uk>
2020-10-16 00:05:41 +0100

    added 3t legos

--- run-scripts.bat
+++ run-scripts.bat
@@ -1,5 +1,6 @@
 .\simc_vs2019.exe .\frost\legendaries.simc
 .\simc_vs2019.exe .\frost\legendaries-2-target.simc
+.\simc_vs2019.exe .\frost\legendaries-3-target.simc
 .\simc_vs2019.exe .\frost\soulbinds-13.simc
 .\simc_vs2019.exe .\frost\soulbinds-25.simc
 .\simc_vs2019.exe .\frost\soulbinds-max.simc
