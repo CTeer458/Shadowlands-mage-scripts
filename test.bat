6bd7a44370b2ce9791eeab9832ec52c4c0a9b779
cteer458 <conorteer@hotmail.co.uk>
2020-11-20 19:54:29 +0000

    updates

--- test.bat
+++ test.bat
@@ -1,3 +1,38 @@
-.\simc.exe .\fire\trinkets.simc
+
+.\simc.exe .\frost\soulbinds-16.simc
+.\simc.exe .\frost\soulbinds-22.simc
+.\simc.exe .\frost\soulbinds-25.simc
+.\simc.exe .\frost\soulbinds-max.simc
+
 .\simc.exe .\frost\trinkets.simc
+
+
+.\simc.exe .\arcane\soulbinds-16.simc
+.\simc.exe .\arcane\soulbinds-22.simc
+.\simc.exe .\arcane\soulbinds-25.simc
+.\simc.exe .\arcane\soulbinds-max.simc
+
 .\simc.exe .\arcane\trinkets.simc
+
+.\simc.exe .\fire\soulbinds-16.simc
+.\simc.exe .\fire\soulbinds-22.simc
+.\simc.exe .\fire\soulbinds-25.simc
+.\simc.exe .\fire\soulbinds-max.simc
+
+.\simc.exe .\fire\trinkets.simc
+
+
+.\simc.exe .\destruction\soulbinds-16.simc
+.\simc.exe .\destruction\soulbinds-25.simc
+.\simc.exe .\destruction\soulbinds-22.simc
+.\simc.exe .\destruction\soulbinds-max.simc
+
+.\simc.exe .\destruction\trinkets.simc
+
+
+.\simc.exe .\affliction\soulbinds-16.simc
+.\simc.exe .\affliction\soulbinds-25.simc
+.\simc.exe .\affliction\soulbinds-22.simc
+.\simc.exe .\affliction\soulbinds-max.simc
+
+.\simc.exe .\affliction\trinkets.simc
